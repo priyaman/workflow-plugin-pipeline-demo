414a2d27e9cbd8b2735a574090b2e309fb6dd9d4
Udaypal Aarkoti <uaarkoti@gmail.com>
2014-12-18 20:23:43 -0800

    Single quotes instead of double

--- flow.groovy
+++ flow.groovy
@@ -1,10 +1,10 @@
-def tomcatHost = "localhost"
-def tomcatPort = "8180"
-def appHost = "http://${tomcatHost}:${tomcatPort}"
-def tomcatUser = "admin"
-def tomcatPassword = "tomcat"
-def tomcatDeployUrl = "http://${tomcatUser}:${tomcatPassword}@${tomcatHost}:${tomcatPort}/manager/deploy"
-def tomcatUndeployUrl = "http://${tomcatUser}:${tomcatPassword}@${tomcatHost}:${tomcatPort}/manager/undeploy"
+def tomcatHost = 'localhost'
+def tomcatPort = '8180'
+def appHost = 'http://${tomcatHost}:${tomcatPort}'
+def tomcatUser = 'admin'
+def tomcatPassword = 'tomcat'
+def tomcatDeployUrl = 'http://${tomcatUser}:${tomcatPassword}@${tomcatHost}:${tomcatPort}/manager/deploy'
+def tomcatUndeployUrl = 'http://${tomcatUser}:${tomcatPassword}@${tomcatHost}:${tomcatPort}/manager/undeploy'
 
 def devQAStaging() {
     env.PATH="${tool 'Maven 3.x'}/bin:${env.PATH}"
