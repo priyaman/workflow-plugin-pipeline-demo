66bbf75378859ffd85add130d2d04506c70aeb12
Udaypal Aarkoti <uaarkoti@gmail.com>
2014-12-18 20:22:07 -0800

    detype

--- flow.groovy
+++ flow.groovy
@@ -1,10 +1,10 @@
-String tomcatHost = "localhost"
-String tomcatPort = "8180"
-String appHost = "http://${tomcatHost}:${tomcatPort}"
-String tomcatUser = "admin"
-String tomcatPassword = "tomcat"
-String tomcatDeployUrl = "http://${tomcatUser}:${tomcatPassword}@${tomcatHost}:${tomcatPort}/manager/deploy"
-String tomcatUndeployUrl = "http://${tomcatUser}:${tomcatPassword}@${tomcatHost}:${tomcatPort}/manager/undeploy"
+def tomcatHost = "localhost"
+def tomcatPort = "8180"
+def appHost = "http://${tomcatHost}:${tomcatPort}"
+def tomcatUser = "admin"
+def tomcatPassword = "tomcat"
+def tomcatDeployUrl = "http://${tomcatUser}:${tomcatPassword}@${tomcatHost}:${tomcatPort}/manager/deploy"
+def tomcatUndeployUrl = "http://${tomcatUser}:${tomcatPassword}@${tomcatHost}:${tomcatPort}/manager/undeploy"
 
 def devQAStaging() {
     env.PATH="${tool 'Maven 3.x'}/bin:${env.PATH}"
